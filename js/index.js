1eeca6e0a785a014d87810fddb0697cf17eb362a
Soham manjrekar <mrsohammanjrekar@gmail.com>
2023-04-08 16:42:08 +0530

    upload

--- js/index.js
+++ js/index.js
@@ -1,84 +1,77 @@
-const resultElement = document.getElementById("result");
-const lengthElement = document.getElementById("length");
-const uppercaseElement = document.getElementById("uppercase");
-const lowercaseElement = document.getElementById("lowercase");
-const numbersElement = document.getElementById("numbers");
-const symbolsElement = document.getElementById("symbols");
-const generateElement = document.getElementById("generate");
-const clipboardElement = document.getElementById("clipboard");
+// include api for currency change
 
-// Random functions
-// fromCharCode: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
-// ASCII codes: https://www.w3schools.com/charsets/ref_html_ascii.asp
-const getRandomLower = () =>
-  String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+const api = 'https://api.exchangerate-api.com/v4/latest/USD'
 
-const getRandomUpper = () =>
-  String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// for selecting different controls
 
-const getRandomNumber = () =>
-  String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+var search = document.querySelector('.searchBox')
 
-const getRandomSymbol = () => {
-  const symbols = "!@#$%^&*(){}[]=<>/,.";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-};
+var convert = document.querySelector('.convert')
 
-const randomFunctions = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomSymbol,
-  symbol: getRandomSymbol,
-};
+var fromCurrecy = document.querySelector('.from')
 
-const createNotification = (message) => {
-  const notif = document.createElement("div");
-  notif.classList.add("toast");
-  notif.innerText = message;
-  document.body.appendChild(notif);
-  setTimeout(() => notif.remove(), 3000);
-};
+var toCurrecy = document.querySelector('.to')
 
-clipboardElement.addEventListener("click", () => {
-  const password = resultElement.innerText;
-  if (!password) return;
-  const textarea = document.createElement("textarea");
-  textarea.value = password;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand("copy");
-  textarea.remove();
-  createNotification("Password copied to clipboard!");
-});
+var finalValue = document.querySelector('.finalValue')
 
-generateElement.addEventListener("click", () => {
-  const length = +lengthElement.value;
-  const hasLower = lowercaseElement.checked;
-  const hasUpper = uppercaseElement.checked;
-  const hasNumber = numbersElement.checked;
-  const hasSymbol = symbolsElement.checked;
-  resultElement.innerText = generatePassword(
-    hasLower,
-    hasUpper,
-    hasNumber,
-    hasSymbol,
-    length
-  );
-});
+var finalAmount = document.getElementById('finalAmount')
 
-const generatePassword = (lower, upper, number, symbol, length) => {
-  let generatedPassword = "";
-  const typesCount = lower + upper + number + symbol;
-  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
-    (item) => Object.values(item)[0]
-  );
-  if (typesCount === 0) return "";
-  for (let i = 0; i < length; i += typesCount) {
-    typesArr.forEach((type) => {
-      const funcName = Object.keys(type)[0];
-      generatedPassword += randomFunctions[funcName]();
-    });
-  }
-  const finalPassword = generatedPassword.slice(0, length);
-  return finalPassword;
-};
+var resultFrom
+
+var resultTo
+
+var searchValue
+
+// Event when currency is changed
+
+fromCurrecy.addEventListener('change', (event) => {
+  resultFrom = `${event.target.value}`
+})
+
+// Event when currency is changed
+
+toCurrecy.addEventListener('change', (event) => {
+  resultTo = `${event.target.value}`
+})
+
+search.addEventListener('input', updateValue)
+
+// function for updating value
+
+function updateValue(e) {
+  searchValue = e.target.value
+}
+
+// when user clicks, it calls function getresults
+
+convert.addEventListener('click', getResults)
+
+// function getresults
+
+function getResults() {
+  fetch(`${api}`)
+    .then((currency) => {
+      return currency.json()
+    })
+    .then(displayResults)
+}
+
+// display results after convertion
+
+function displayResults(currency) {
+  let fromRate = currency.rates[resultFrom]
+
+  let toRate = currency.rates[resultTo]
+
+  finalValue.innerHTML = ((toRate / fromRate) * searchValue).toFixed(2)
+
+  finalAmount.style.display = 'block'
+}
+
+// when user click on reset button
+
+function clearVal() {
+  window.location.reload()
+
+  document.getElementsByClassName('finalValue').innerHTML = ''
+}
